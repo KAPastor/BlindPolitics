948d82c5d1a2fe9ad4c3105024d7473bda2e1c54
Kyle Pastor <kyleanthonypastor@gmail.com>
2017-10-26 15:12:42 -0400

    aaa

--- web/app.js
+++ web/app.js
@@ -5,9 +5,21 @@ var path    = require("path");
 var PythonShell = require('python-shell');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 
 app.use("/public", express.static(path.join(__dirname, 'public')));
